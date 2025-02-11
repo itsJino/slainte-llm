7daa40029ffe679f40bba5de1cd5fad079d41501
joshuaflores <floresjoshua2603@gmail.com>
2025-02-11 21:34:20 +0000

    Initial Commit

---
+++ backend/slainte/src/test/java/com/example/slainte/SlainteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slainte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlainteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
